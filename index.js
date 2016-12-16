ddf779cb7df90672b67595027cde41cc597465af
Artur Nista <arturnista@gmail.com>
2016-12-16 15:26:08 -0200

    Fixed night jump

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const robot = require('robotjs')
 
 const mouse = { x: 2224, y: 1210 }
 
+function isBlack(c) {
+    c = c.substring(0, 2)
+    return parseInt(c, 16) < 150
+}
+
 function haveToJump(x, y) {
-    let lastHex = ''
+    let lastHex = '1'
     for (let off = 20; off >= 0; off -= 10) {
         const hex = robot.getPixelColor(x, y + off)
+        const bl = isBlack(hex)
         // if(hex === '535353' || hex === 'acacac') return true
-        if(lastHex !== hex) {
-            if(lastHex !== '') return true
-            lastHex = hex
+        if(lastHex !== bl) {
+            if(lastHex !== '1') return true
+            lastHex = bl
         }
     }
     return false
@@ -23,6 +29,7 @@ function checkEndGame() {
 let jumpTime = 0
 let downTime = 0
 let downWaiting = 0
+let treeAhead = false
 let objectUnder = false
 
 let endGameCheck = 0
@@ -42,26 +49,32 @@ while(true) {
     }
     let min = pos / 50 // 50
     min = min < 40 ? 40 : min
-    let max = pos / 28 // 70
-    max = max < 70 ? 70 : max
-    for (let i = min; i <= max; i += 10) {
+
+    // let max = pos / 28 // 70
+    // max = max < 70 ? 70 : max
+    for (let i = min; i <= min + 30; i += 10) {
         if(haveToJump(mouse.x + i, mouse.y)) {
             // robot.keyTap('space')
             robot.keyToggle('space', 'down')
             jumpTime = 1
             objectUnder = false
+            treeAhead = true
             break
+        } else {
+            treeAhead = false
         }
     }
 
     if(jumpTime > 0) {
         const ref = robot.getPixelColor(mouse.x, mouse.y + 12)
-        if(pos > 1500 && ref === '535353' || ref === 'acacac') {
+        if(pos > 1500 && (ref === '535353' || ref === 'acacac')) {
             objectUnder = true
         } else if(objectUnder) {
             downWaiting = 1
             objectUnder = false
         }
+
+        if(treeAhead && downWaiting > 0 && downWaiting < 4) downWaiting = 3 // Set to 3 so will enter in the if
 
         if(++jumpTime >= 10) {
             robot.keyToggle('space', 'up')
--- mpc.js
+++ mpc.js
@@ -1,11 +1,25 @@
 const robot = require('robotjs')
 
-let colors = []
-while (true) {
-    const mouse = robot.getMousePos()
-    let hex = robot.getPixelColor(mouse.x, mouse.y)
-    if(colors.indexOf(hex) === -1) {
-        colors.push(hex)
-        console.log(hex)
-    }
-}
+// const color = 'ff'
+// function isBlack(c) {
+//     c = c.substring(0, 2)
+//     return parseInt(c, 16) < 150
+// }
+// let aaa = 0
+// while (true) {
+//     const mouse = robot.getMousePos()
+//     let hex = robot.getPixelColor(mouse.x, mouse.y)
+//     if(isBlack(hex)) {
+//         console.log('é preto' + ++aaa)
+//     }
+// }
+
+// let colors = []
+// while (true) {
+//     const mouse = robot.getMousePos()
+//     let hex = robot.getPixelColor(mouse.x, mouse.y)
+//     if(colors.indexOf(hex) === -1) {
+//         colors.push(hex)
+//         console.log(hex)
+//     }
+// }
